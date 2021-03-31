ae09f94aa98f4c8fc37b33c62a5a09867cb63165
anmolvashistha <anmolvashisthaav@gmail.com>
2021-03-31 14:57:02 +0530

    user list

--- src/app/users/users/users-list.component.ts
+++ src/app/users/users/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-users-list',
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
+  columnDefs = [
+    {field: 'id', width: 150},
+    {field: 'fname', headerName: 'First Name', sortable: true, filter: true},
+    {field: 'lname', headerName: 'Last Name', sortable: true, filter: true},
+    {field: 'email', sortable: true, filter: true},
+    {field: 'contact'},
+    // {field: 'createdAt'},
+  ];
 
-  constructor() { }
+  rowData = [
+    {fname: 'Anmol', lname: 'Vashistha', email: 'av@gmail.com', contact: '8899889988', id: 1},
+    {fname: 'John', lname: 'Doe', email: 'johndoe@gmail.com', contact: '7755775577', id: 2},
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
