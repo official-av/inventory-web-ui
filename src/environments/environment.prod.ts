f68a21fdaa21d9ef40cb43a0ce977dce3c35a884
Anmol Vashistha <anmolvashisthaav@gmail.com>
2021-04-09 17:16:37 +0530

    update env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL: 'https://5ilqh2z1yf.execute-api.us-east-2.amazonaws.com/dev',
+  pwdRegex: '(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}'
 };
