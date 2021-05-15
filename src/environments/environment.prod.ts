a9069771eb155b324d6a2e1c375186d15e513d8a
Anmol Vashistha <anmolvashisthaav@gmail.com>
2021-05-15 18:51:44 +0530

    updated API base URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: 'https://5ilqh2z1yf.execute-api.us-east-2.amazonaws.com/dev',
+  baseURL: 'https://pln10fp830.execute-api.us-east-1.amazonaws.com/dev',
   pwdRegex: '(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   // baseURL: 'http://localhost:3000/dev',
-  baseURL: 'https://5ilqh2z1yf.execute-api.us-east-2.amazonaws.com/dev',
+  baseURL: 'https://pln10fp830.execute-api.us-east-1.amazonaws.com/dev',
   pwdRegex: '(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
